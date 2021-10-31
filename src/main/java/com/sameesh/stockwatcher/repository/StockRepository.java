bc735069021af9d86798f06e8ff7901a4124add6
sameesh-s <sameesh.fisat@gmail.com>
2021-10-31 08:55:23 +0530

    wishlist retrival working

--- src/main/java/com/sameesh/stockwatcher/repository/StockRepository.java
+++ src/main/java/com/sameesh/stockwatcher/repository/StockRepository.java
@@ -10,7 +10,6 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public class StockRepository {
--- src/main/java/com/sameesh/stockwatcher/service/WishListService.java
+++ src/main/java/com/sameesh/stockwatcher/service/WishListService.java
@@ -1,6 +1,7 @@
 package com.sameesh.stockwatcher.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sameesh.stockwatcher.controller.WishListController;
 import com.sameesh.stockwatcher.dto.Stock;
 import com.sameesh.stockwatcher.dto.WishList;
 import com.sameesh.stockwatcher.entity.StockEntity;
@@ -10,6 +11,8 @@ import com.sameesh.stockwatcher.repository.StockRepository;
 import com.sameesh.stockwatcher.repository.WishListRepository;
 import com.sameesh.stockwatcher.strategy.StockSelectionStrategy;
 import com.sameesh.stockwatcher.strategy.StockStrategyFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,6 +25,8 @@ import java.util.stream.Collectors;
 public class WishListService {
 
     private static final ObjectMapper mapper = new ObjectMapper();
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WishListService.class);
 
     @Autowired
     WishListRepository wishListRepository;
@@ -65,11 +70,18 @@ public class WishListService {
 
     public WishList create(WishList wishList) {
         List<Stock> stockDtos = wishList.getStocks();
+        LOGGER.warn("Wishlist " + wishList.getName() + " has stock size : " + stockDtos.size());
         WishListEntity wishListEntity = mapper.convertValue(wishList, WishListEntity.class);
         if(stockDtos.size() > 0){
+            wishList.setStocks(
             stockDtos.stream()
-                    .forEach(item -> item = mapper.convertValue(stockRepository.save(mapper.convertValue(item, StockEntity.class)), Stock.class));
-            wishListEntity.setStockIds(stockDtos.stream().map(Stock::getId).collect(Collectors.toList()));
+                    .map(item ->{
+                        item = mapper.convertValue(stockRepository.save(mapper.convertValue(item, StockEntity.class)), Stock.class);
+                        return item;
+                    })
+                    .collect(Collectors.toList())
+            );
+            wishListEntity.setStockIds(wishList.getStocks().stream().map(Stock::getId).collect(Collectors.toList()));
         }
         wishList.setId(wishListRepository.save(wishListEntity).getId());
         return wishList;
